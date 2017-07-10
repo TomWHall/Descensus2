eefea80de22931b933376419f6834685ad9e328d
Tom W Hall <tomshalls@gmail.com>
2017-07-10 22:57:15 +1200

    Deleted generated .js files

--- src/js/p2/materials.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const materials = {
-    ball: new p2.Material(),
-    wood: new p2.Material(),
-    brick: new p2.Material(),
-    saw: new p2.Material()
-};
-exports.default = materials;
